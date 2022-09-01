fcef58916afb8761d7e2096f284a9f6747bc8fb6
LuanCalais <luancalais@gmail.com>
2022-09-01 14:26:32 -0300

    estudar ciclos de vida de componentes, doCheck e seus filhos

--- Inicial-Modules/meu-primeiro-projeto/src/app/app.component.ts
+++ Inicial-Modules/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core'; //Define um componente
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core'; //Define um componente
 
 //Baseado em um decorator
 @Component({
   selector: 'app-root', //Nome que define a tag personalizada do componente
-  template: '<router-outlet></router-outlet>' // Hack Organização do componentes
+  template: `
+  {{valor}}
+  <button (click)="adicionar()">ADD</button>
+  <app-title title='Aula ciclo de vida de componentes'></app-title>
+  <router-outlet></router-outlet>` // Hack Organização do componentes
 })
-export class AppComponent implements OnInit {
-  constructor() { }
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit {
+  
+  public valor:number = 1
 
+  constructor() { } 
+
+  // Função para estudo e testes adicionado ao evento
+  public adicionar():number{
+    return this.valor += 1
+  }
+
+  // Arrow Function responsável por fazer o log condensado em um só lugar para debugs e afins
   log: any = (texto: string) => {
     console.log(`${texto}`)
   }
@@ -17,6 +30,34 @@ export class AppComponent implements OnInit {
     setTimeout(() => {
       this.log('NgOnIniti Criado')
     }, 5000);
-
   }
+
+  // ngDoCheck - Este evento é disparado sempre que as propriedades de entrada de um componente são verificadas
+  ngDoCheck(): void {
+    this.log('DoCheck Criado')
+  }
+
+  // Filhos doo ngDoCheck -------------------------------
+
+  // ngAfterContentInit - Este evento é disparado quando o angular realiza qualquer projeção de conteúdo nas visualizações do componente 
+  ngAfterContentInit(): void {
+    this.log('ngAfterContentInit Criado')
+  }
+  
+  //ngAfterContentChecked - Este evento é disparado sempre que o conteúdo do componente é verificado pelo mecanismo de detectção de alteração do Angular, por exemplo='1+1=2' invocaria esse evento
+  ngAfterContentChecked(): void {
+    this.log('ngAfterContentChecked Criado')
+  }
+  
+  
+  //ngAfterViewInit - Esse evento é executado quando a visualização do componente foi totalmente inicializada(Tudo que você pretendia mostrar já foi criado)'
+  ngAfterViewInit(): void {
+    this.log('ngAfterViewInit Criado')
+  }
+  
+  // ngAferViewChecked - Esse evento é executado quando a visualzação de um componente foi verificada pelo algoritmo de detecção de aleração do Angular
+  ngAfterViewChecked(): void {
+    this.log('ngAfterViewChecked Criado')
+  }
+  
 }
--- Inicial-Modules/meu-primeiro-projeto/src/app/title/title.component.ts
+++ Inicial-Modules/meu-primeiro-projeto/src/app/title/title.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
 
-  public title:string = 'Olá Mundo'
+ @Input() public title:string = 'Olá Mundo'
 
   constructor() { }
+  
+  
+  // ngOnChanges - Tem seu início antes do ngOnInit pois ele irá verificar e tratar possíveis dados trazidos por variáveis externas, marcadas palo decorator @Input, sem o title sendo setado no app.component ele não é ativado
+  ngOnChanges(): void{
+    console.log('ngOnChanges - informação alterada')
+  }
 
-  // Ciclo de vida do componente
+  // ngOnInit - Ciclo de vida do componente(Explicado no app.component.ts)
   ngOnInit(): void {
-
+    console.log('ngOnInit do title component iniciado')
   }
 
 }
+
