4e91b8ce987e739e34b7c37bac253c1df0c331d5
LuanCalais <luancalais@gmail.com>
2022-09-01 13:28:10 -0300

    voltar master para configuracao inicial

--- Inicial-Modules/meu-primeiro-projeto/src/app/app.component.ts
+++ Inicial-Modules/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit } from '@angular/core'; //Define um componente
+import { Component } from '@angular/core'; //Define um componente
 
 //Baseado em um decorator
 @Component({
   selector: 'app-root', //Nome que define a tag personalizada do componente
   template: '<router-outlet></router-outlet>' // Hack Organização do componentes
 })
-export class AppComponent implements OnInit{
-  constructor(){}
-
-  ngOnInit{}
+export class AppComponent {
 }
