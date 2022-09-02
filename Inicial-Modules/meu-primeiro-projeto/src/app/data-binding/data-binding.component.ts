12b387d3aa77406fe3c6e00cd7a8337ca86d65e2
LuanCalais <luancalais@gmail.com>
2022-09-02 15:46:33 -0300

    Property Binding

--- Inicial-Modules/meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ Inicial-Modules/meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -10,6 +10,11 @@ export class DataBindingComponent implements OnInit {
   public nome:string = 'Luan Calais'
   public idade:number = 26
 
+  // Property Biding
+  public checkedDisabled: boolean = true
+  public imgSrc: string = 'https://lupadigital.info/wp-content/uploads/2018/05/imagens-gratis.jpg'
+  public imgTitle: string = 'background'
+
   constructor() { }
 
   ngOnInit(): void {
