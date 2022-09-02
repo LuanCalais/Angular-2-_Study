5904886c81822e379341d168f08008d186f68a87
LuanCalais <luancalais@gmail.com>
2022-09-02 16:17:46 -0300

    event binding

--- Inicial-Modules/meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ Inicial-Modules/meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  // Interpolation
   public nome:string = 'Luan Calais'
   public idade:number = 26
 
@@ -15,9 +16,23 @@ export class DataBindingComponent implements OnInit {
   public imgSrc: string = 'https://lupadigital.info/wp-content/uploads/2018/05/imagens-gratis.jpg'
   public imgTitle: string = 'background'
 
+  // Event Biding
+  public position: {x:number, y: number} = {x: 0, y: 0} 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Event Binding
+  public alertaInfo(valor:MouseEvent):void{
+    // Recebe vários eventos de mouse usando a variável $event no evento de (click)
+    console.log(valor)
+  }
+
+  public mouseMoveTest(valor:MouseEvent):void{
+    this.position.x = valor.offsetX
+    this.position.y = valor.offsetY
   }
 
 }
