f2ae64da093ae6de47b77898c3b1c5274ecf2de1
LuanCalais <luancalais@gmail.com>
2022-09-05 17:28:02 -0300

    anotar informacoes estrutura module

--- Inicial-Modules/meu-primeiro-projeto/src/app/app.module.ts
+++ Inicial-Modules/meu-primeiro-projeto/src/app/app.module.ts
@@ -6,15 +6,23 @@ import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 
 @NgModule({
+  // Declara componentes dentro desse módulo
   declarations: [
     AppComponent,
     TitleComponent
   ],
+
+  // Importa funcionalidades de outros componentes
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+
+  // Declara services para requisições ao backend
   providers: [],
+
+  // Apenas utilizado no componente principal, indica que é a parte do aplicativo que vai startar toda a aplicação(appStarter)
   bootstrap: [AppComponent]
 })
+// Exporta componentes 
 export class AppModule { }
