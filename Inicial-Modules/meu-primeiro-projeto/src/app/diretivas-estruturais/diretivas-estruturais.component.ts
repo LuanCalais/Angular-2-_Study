765e2d3b8a84982eb51f71be0dfe32c0e251e003
LuanCalais <luancalais@gmail.com>
2022-09-04 21:24:28 -0300

    estudar ngfor

--- Inicial-Modules/meu-primeiro-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ Inicial-Modules/meu-primeiro-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -7,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasEstruturaisComponent implements OnInit {
 
-  public condition:boolean = true
+  public condition: boolean = true
   public conditionClick: boolean = true
+
+  // Tipo lista que receve um objeto com o parâmetro nome do tipo string
+  public list: Array<{ nome: string, idade: number }> = [
+    { nome: "Luan", idade: 26 },
+    { nome: "Marilia", idade: 23 },
+    { nome: "Emma", idade: 1 }
+  ]
 
   constructor() { }
 
@@ -16,9 +23,9 @@ export class DiretivasEstruturaisComponent implements OnInit {
 
     setInterval(() => {
 
-      if(this.condition === true){
+      if (this.condition === true) {
         this.condition = false
-      }else if(this.condition === false){
+      } else if (this.condition === false) {
         this.condition = true
       }
 
@@ -26,12 +33,27 @@ export class DiretivasEstruturaisComponent implements OnInit {
 
   }
 
-  public onClick():void{
-    if(this.conditionClick === true){
+  public onClick(): void {
+    if (this.conditionClick === true) {
       this.conditionClick = false
-    }else if(this.conditionClick === false){
+    } else if (this.conditionClick === false) {
       this.conditionClick = true
     }
+  }
+
+  public onClickAddList(): void{
+    this.list.push({nome: "Thalles", idade: 25})
+  }
+
+  public onClickRemoveList(): void{
+    this.list.pop()
+  }
+
+  public onClickEventList(e:MouseEvent, index:number): void{
+    console.log(index)
+    
+    // Remove esse item da lista
+    this.list.splice(index, 1)
   }
 
 }
