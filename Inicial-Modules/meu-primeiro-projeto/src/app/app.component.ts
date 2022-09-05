35260d4ecdb1286a50f527125ccde961684da613
LuanCalais <luancalais@gmail.com>
2022-09-05 16:46:34 -0300

    estudar ng-content

--- Inicial-Modules/meu-primeiro-projeto/src/app/app.component.ts
+++ Inicial-Modules/meu-primeiro-projeto/src/app/app.component.ts
@@ -5,7 +5,21 @@ import { Component } from '@angular/core'; //Define um componente
   selector: 'app-root',
   template: `
   <app-diretivas-estruturais></app-diretivas-estruturais>
-  <app-diretivas-atributo></app-diretivas-atributo>
+  
+  <app-diretivas-atributo>
+  <h1>Diretivas de Atributo</h1>
+  <hr>
+  <h3>Conteúdo Final</h3>
+  </app-diretivas-atributo>
+  
+  <app-diretivas-atributo>
+  <h1>Teste de NG-CONTENT</h1>
+  <hr>
+  <h3>Olá mundo</h3>
+  </app-diretivas-atributo>
+
+
+
   <router-outlet></router-outlet>
   `
 })
