5bb7e872ff104f97cbb7e1874a3aa4d702e569f2
LuanCalais <luancalais@gmail.com>
2022-09-04 22:10:14 -0300

    estudar ng-style

--- Inicial-Modules/meu-primeiro-projeto/src/app/diretivas-atributo/diretivas-atributo.component.ts
+++ Inicial-Modules/meu-primeiro-projeto/src/app/diretivas-atributo/diretivas-atributo.component.ts
@@ -8,17 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class DiretivasAtributoComponent implements OnInit {
 
   public valor: boolean = true
+  public height: string = "20px"
+  public border: string = "1px solid red"
 
   constructor() { }
 
   ngOnInit(): void {
 
     setInterval(() => {
+
       if (this.valor) {
         this.valor = false
       } else {
         this.valor = true
       }
+
+      if(this.height === "20px"){
+        this.height = "100px"
+        this.border = '1px dashed blue'
+
+      }else{
+        this.height = "50px"
+        this.border = '1px dotted green'
+      }
+
     }, 2000)
 
   }
